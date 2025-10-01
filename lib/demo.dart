37052438e674b09e8cab95b5a3327cc045ed2759
Kapil_Mukati <kapil.mukati@yunicorn.in>
2025-10-01 17:01:25 +0530

    Container_widget practice code

---
+++ lib/demo.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:training/advance_dart/generics.dart';
+class DemoClass extends StatelessWidget {
+  const DemoClass({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // return Stack(
+    //   alignment: Alignment.center,
+    //   children: [
+    //     // Shadow layer
+    //     Positioned(
+    //       bottom: -10, // card ke neeche distance
+    //       child: Container(
+    //         width: 160,  // card se thodi badi
+    //         height: 20,
+    //         decoration: BoxDecoration(
+    //           color: Colors.black.withOpacity(0.25), // soft shadow
+    //           borderRadius: BorderRadius.circular(50), // elliptical shadow
+    //           boxShadow: [
+    //             BoxShadow(
+    //               color: Colors.black.withOpacity(0.3),
+    //               blurRadius: 15,
+    //               spreadRadius: 5,
+    //               offset: Offset(0, 0),
+    //             ),
+    //           ],
+    //         ),
+    //       ),
+    //     ),
+    //     Positioned(
+    //       top: -10, // card ke neeche distance
+    //       child: Container(
+    //         width: 160,  // card se thodi badi
+    //         height: 20,
+    //         decoration: BoxDecoration(
+    //           color: Colors.black.withOpacity(0.25), // soft shadow
+    //           borderRadius: BorderRadius.circular(50), // elliptical shadow
+    //           boxShadow: [
+    //             BoxShadow(
+    //               color: Colors.black.withOpacity(0.3),
+    //               blurRadius: 15,
+    //               spreadRadius: 5,
+    //               offset: Offset(0, 0),
+    //             ),
+    //           ],
+    //         ),
+    //       ),
+    //     ),
+    //     // Actual Card
+    //     Container(
+    //       width: 150,
+    //       height: 100,
+    //       decoration: BoxDecoration(
+    //         color: Colors.white,
+    //         borderRadius: BorderRadius.circular(15),
+    //       ),
+    //       child: Center(child: Text("Rectangle Card")),
+    //     ),
+    //   ],
+    // )
+    // ;
+
+    // return Container(
+    //   width: 150,
+    //   height: 150,
+    //   decoration: BoxDecoration(
+    //     color: Colors.white,
+    //     boxShadow: [
+    //       BoxShadow(
+    //         color: Colors.black12, // light, diffuse shadow
+    //         blurRadius: 10,
+    //         offset: Offset(0, 2),
+    //       ),
+    //       BoxShadow(
+    //         color: Colors.black26, // stronger shadow
+    //         blurRadius: 20,
+    //         offset: Offset(0, 10),
+    //       ),
+    //       BoxShadow(
+    //         color: Colors.black38, // darkest shadow near contact point
+    //         blurRadius: 30,
+    //         offset: Offset(0, 20),
+    //       ),
+    //     ],
+    //     borderRadius: BorderRadius.circular(12),
+    //   ),
+    // );
+
+
+    return Stack(
+      children: [
+        Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                offset: Offset(0, 10),
+                blurRadius: 10,
+                spreadRadius: 2,
+              ),
+            ],
+            shape: BoxShape.circle,
+            gradient: LinearGradient(
+              colors: [Colors.blue.shade200, Colors.lightGreen.shade200],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        ),
+        Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: RadialGradient(
+              colors: [Colors.orangeAccent.withOpacity(0.5), Colors.orange.withOpacity(0.4), Colors.transparent],
+              center: Alignment.bottomLeft,
+            ),
+          ),
+        ),
+        // Container(
+        //   width: 200,
+        //   height: 200,
+        //   decoration: BoxDecoration(
+        //     shape: BoxShape.circle,
+        //     gradient: RadialGradient(
+        //       colors: [Colors.orangeAccent.withOpacity(0.5), Colors.orange.withOpacity(0.4), Colors.transparent],
+        //       center: Alignment.topRight,
+        //       radius: 0.5,
+        //       tileMode: TileMode.decal,
+        //     ),
+        //   ),
+        // ),
+        // Container(
+        //   width: 200,
+        //   height: 200,
+        //   decoration: BoxDecoration(
+        //     // shape: BoxShape.circle,
+        //     gradient: SweepGradient(colors: [Colors.red, Colors.blue])
+        //
+        //   ),
+        // )
+      ],
+    );
+
+  }
+}
---
+++ lib/flutter_widgets/container_widget.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+
+class ContainerClass extends StatelessWidget {
+  const ContainerClass({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // var size = MediaQuery.of(context).size;
+
+    return Stack(
+      children: [
+        Container(
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          height: 130,
+          width: 120,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: LinearGradient(colors: [Colors.red, Colors.blue]),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          height: 140,
+          width: 120,
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            color: Colors.purple,
+            // gradient: LinearGradient(
+            //   colors: [Colors.blue, Colors.purple, Colors.orangeAccent],
+            //   begin: Alignment.topLeft,
+            //   end: Alignment.bottomRight,
+            //   stops: [0.2, 0.3, 1.0],
+            //   // transform: GradientRotation(3.14),
+            // ),
+            // border: Border(bottom: BorderSide(color: Colors.red)),
+            gradient: RadialGradient(
+              colors: [
+                Colors.deepOrange,
+                Colors.orangeAccent,
+                Colors.blue.shade100,
+              ],
+              center: Alignment.bottomLeft,
+              radius: 0.6,
+            ),
+            // shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.25),
+                offset: Offset(0, 8),
+                blurRadius: 15,
+                spreadRadius: 1,
+              ),
+
+              // ========================
+              // BoxShadow(
+              //   color: Colors.blue,
+              //   offset: Offset(0, 0),
+              //   spreadRadius: 10,
+              // ),
+              // BoxShadow(
+              //   color: Colors.grey.withOpacity(0.5),
+              //   offset: Offset(0, 100),
+              //   spreadRadius: -18,
+              //   blurRadius: 30,
+              //
+              // ),
+              // BoxShadow(
+              //   color: Colors.redAccent,
+              //   offset: Offset(-9, 0),
+              //   spreadRadius: 10,
+              // ),
+              // BoxShadow(
+              //     color: Colors.blue,
+              //     offset: Offset(15, 0),
+              //     spreadRadius: 10,
+              // ),
+              // BoxShadow(
+              //     color: Colors.redAccent,
+              //     offset: Offset(9, 0),
+              //     spreadRadius: 10,
+              // )
+            ],
+          ),
+
+          child: Text('Container'),
+        ),
+      ],
+    );
+  }
+}
+
+class NewContainerClass extends StatelessWidget {
+  const NewContainerClass({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      width: 200,
+      alignment: Alignment.topRight,
+      padding: EdgeInsets.only(top: 20, right: 20),
+      child: Text("Container1"),
+      decoration: BoxDecoration(
+        color: Colors.red,
+        // border: Border.all(
+        //   color: Colors.green,
+        //   width: 2,
+        //   // strokeAlign: BorderSide.strokeAlignOutside,
+        //   // strokeAlign: BorderSide.strokeAlignInside
+        //   // strokeAlign: BorderSide.strokeAlignCenter,
+        //
+        // ),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.elliptical(20, 10),
+          bottomLeft: Radius.circular(20),
+          bottomRight: Radius.elliptical(10, 20),
+        ),
+        // borderRadius: BorderRadius.all(
+        //   Radius.elliptical(10, 50),
+        //
+        // )
+      ),
+      // foregroundDecoration: BoxDecoration(
+      //
+      // )
+      // rotate..
+      // transform: Matrix4.rotationZ(0.2),
+      // transform: Matrix4.translationValues(0,0,0),
+
+      // Skew..
+      // transform: Matrix4.skewX(0.3),
+
+    //   combine all
+      transform: Matrix4.identity()
+        ..translate(30.0, 10.0)
+        ..rotateZ(0.5)
+        ..scale(0.9),
+
+
+    );
+
+  }
+}
